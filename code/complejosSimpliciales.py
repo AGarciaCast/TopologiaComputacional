d45794c69d5f697283dd6b292e7530e768b67d59
Alejandro <a.gcastellanos@alumnos.upm.es>
2020-10-30 20:36:13 +0100

    Eliminar espacios troll

--- code/complejosSimpliciales.py
+++ code/complejosSimpliciales.py
@@ -44,7 +44,7 @@ def puntosCurvaRuido(curva, t, t0, t1, numPuntos=10, mu=0, sigma=0.1):
 def pivotar(M, k, m):
     """
     Pivota en el elemento (k,m) intercambiando filas y columnas.
-    
+
     M: np.array.
     k: int.
     m: int.
@@ -74,7 +74,7 @@ def pivotar(M, k, m):
 def sumaFilZ2(M, i, j):
     """
     Suma: filai + filaj (sobre la j).
-    
+
     M: np.array.
     i: int.
     j: int.
@@ -85,7 +85,7 @@ def sumaFilZ2(M, i, j):
 def sumaColZ2(M, i, j):
     """
     Suma: coli + colj (sobre la j).
-    
+
     M: np.array.
     i: int.
     j: int.
@@ -96,7 +96,7 @@ def sumaColZ2(M, i, j):
 def normSmithZ2(M):
     """
     Obtener la forma normal de Smith de una matriz con coefs en Z2.
-    
+
     M: np.array.
     """
     n = 0
@@ -139,7 +139,7 @@ def ordCaras(cara):
 def distancia(p1, p2):
     """
     Distancia euclídea entre los puntos p1 y p2.
-    
+
     p1: list.
     p2: list.
     """
@@ -168,7 +168,7 @@ def radioCircunscrita(p1, p2, p3):
 def analisisComplejo(comp, simplice):
     """
     Alisis de las propiedades del complejo simplicial.
-    
+
     comp: Complejo.
     simplice: set(tuple)
         simplice como ref para ejemplos.
@@ -275,7 +275,7 @@ def alfaComplejo(puntos):
 def plotalpha(puntos, K):
     """
     Representar el alpha complejo del complejo K.
-    
+
     puntos: np.array.
     K: Complejo.
     """
@@ -301,7 +301,7 @@ def plotalpha(puntos, K):
 def vietorisRips(puntos):
     """
     Calculo del complejo Vietoris Rips de una nube de puntos.
-    
+
     puntos: np.array.
     """
     nsimplex = Complejo([tuple(range(len(puntos)))])
@@ -317,44 +317,44 @@ def vietorisRips(puntos):
             for arista in combinations(simplex, 2):
                 lista.append(0.5 * distancia(puntos[arista[0]], puntos[arista[1]]))
             VR.insert([simplex], max(lista))
-    
+
     return VR
 
 
 class Complejo():
     """Clase del complejo simplicial."""
-    
+
     def __init__(self, carasMaximales=[]):
         """
         Complejo simplicial abstracto a partir de sus caras maximales.
-        
+
         carasMaximales: list(tuple). Por defecto [].
         """
         # Concatenamos el los conjuntos obtenidos de cada cara maximal
         self.caras = set()
         for cara in carasMaximales:
             if cara not in self.caras:
-                self.caras |= set(powerset(cara)) 
+                self.caras |= set(powerset(cara))
         # Quitamos el conjunto vacio
         self.caras -= {()}
-        
+
         # Añadimos peso
         self.caras = set([(cara, 0.0) for cara in self.caras])
-        
+
         self.carasOrd = sorted(list(self.caras), key=ordCaras)
-        
+
     def setCaras(self, carasNuevas, peso=0.0):
         """
         Insertar nuevas caras y sus correspondientes subconjuntos con un peso dado.
-        
+
         carasNuevas: list(tuple).
         peso: float. Por defecto 0.0.
         """
-        for cara in carasNuevas: 
+        for cara in carasNuevas:
             for caraGen in powerset(cara):
                 if caraGen == tuple():
                     continue
-                
+
                 encontrado = False
                 for caraAnt in self.caras:
                     if caraGen == caraAnt[0]:
@@ -362,14 +362,14 @@ class Complejo():
                         if caraAnt[1] > peso:
                             self.caras -= {caraAnt}
                             self.caras |= {(caraGen, peso)}
-                        
+
                         break
-            
+
                 if not encontrado:
                     self.caras |= {(caraGen, peso)}
-        
+
         self.carasOrd = sorted(self.caras, key=ordCaras)
-                
+
     def getCaras(self):
         """Devuelve el conjunto de todas las caras del complejo simplicial."""
         return set([cara[0] for cara in self.caras])
@@ -377,15 +377,15 @@ class Complejo():
     def getCarasOrd(self):
         """Devuelve el conjunto de las caras ordenadas segun su filtracion."""
         return [cara[0] for cara in self.carasOrd]
-    
+
     def umbrales(self):
         """Devuelve el conjunto de las umbrales ordenados segun la filtracion."""
         return list(dict.fromkeys([cara[1] for cara in self.carasOrd]))
-    
+
     def umbral(self, cara):
         """
         Obtiene el umbral de una cara dada.
-        
+
         cara: tuple.
         """
         index = 0
@@ -393,49 +393,49 @@ class Complejo():
         while index < len(self.carasOrd) and not encontrado:
             encontrado = self.carasOrd[index][0] == cara
             index += int(not encontrado)
-        
+
         return self.carasOrd[index][1] if encontrado else None
-    
+
     def dim(self):
         """Devuelve la dimensión del complejo simplicial."""
         return max([len(caras[0]) for caras in self.caras]) - 1
-    
+
     def getCarasN(self, dimension):
         """
         Devuelve el conjunto de todas las caras de dimension dada.
-        
+
         dimension: int.
         """
         return set(c for c in self.getCaras() if len(c) == dimension + 1)
-    
+
     def st(self, v):
         """
         Calcular la estrella del simplice v.
-        
+
         v: set.
         """
         return set(c for c in self.getCaras() if v.issubset(c))
-    
+
     def lk(self, v):
         """
         Calcular el de un simplice v.
-        
+
         v: set.
         """
         # Calculamos la estrella de v
         st = self.st(v)
-        
+
         # Calculamos la estrella cerrada de v
         st_ = set()
         for cara in st:
             if cara not in st_:
-                st_ |= set(powerset(cara)) 
+                st_ |= set(powerset(cara))
         # Quitamos el conjunto vacio
         st_ -= {()}
-        
+
         # Devolvemos el link de v
         return st_ - st
-        
+
     def compConexas(self):
         """Comprobar la conexion de un complejo simplicial."""
         # Para ello comprobamos que su 1-esqueleto sea conexo
@@ -443,15 +443,15 @@ class Complejo():
         k1Graph.add_nodes_from([vertice[0] for vertice in self.getCarasN(0)])
         k1Graph.add_edges_from(self.getCarasN(1))
         return nx.number_connected_components(k1Graph)
-    
+
     def k_esqueleto(self, k):
         """
         Calcular el k-esqueleto de un complejo simplicial.
-        
+
         k: int.
         """
         return set(c for c in self.getCaras() if len(c) <= k + 1)
-    
+
     def drawK1(self):
         """Representación gráfica del 1-esqueleto."""
         k1Graph = nx.Graph()
@@ -463,11 +463,11 @@ class Complejo():
     def caractEuler(self):
         """Obtención de la característica de Euler."""
         return sum([(-1)**k * len(self.getCarasN(k)) for k in range(self.dim() + 1)])
-    
+
     def filtracion(self, a):
         """
         Obtener las caras con peso menor o igual que un valor.
-        
+
         a: float.
         """
         i = 0
@@ -475,22 +475,22 @@ class Complejo():
         while i < len(self.carasOrd) and self.carasOrd[i][1] <= a:
             caras.append(self.carasOrd[i])
             i += 1
-        
+
         result = Complejo()
         for cara, peso in caras:
             result.setCaras([cara], peso)
-            
+
         return result
-    
+
     def matrizBorde(self, p):
         """
         Calculo de la matriz borde de dimensión dada.
-        
+
         p: int.
         """
         if p < 0:
             return None
-        
+
         carasP = sorted(list(self.getCarasN(p)))
 
         if p == 0:
@@ -508,13 +508,13 @@ class Complejo():
                     caraP = set(carasP[j])
                     for i in range(len(carasP_1)):
                         m[i, j] = int(set(carasP_1[i]).issubset(caraP))
-        
+
         return m
-    
+
     def betti(self, p):
         """
         Calculo del número de p de Betti.
-        
+
         p: int.
         """
         if p == 0:
@@ -522,11 +522,11 @@ class Complejo():
         else:
             Mp = normSmithZ2(self.matrizBorde(p))
             Zp = Mp.shape[1] - matrix_rank(Mp)
-        
+
         Bp = matrix_rank(normSmithZ2(self.matrizBorde(p + 1)))
-        
+
         return Zp - Bp
-    
+
     def allBettis(self):
         """Calculo de todos los números de Betti."""
         Zps = np.array([len(self.getCarasN(0))], dtype=int)
@@ -534,14 +534,14 @@ class Complejo():
         d = self.dim()
         for p in range(1, d + 2):
             Mp = normSmithZ2(self.matrizBorde(p))
-            
+
             if p <= d:
                 Zps = np.append(Zps, Mp.shape[1] - matrix_rank(Mp))
-        
+
             Bps = np.append(Bps, matrix_rank(Mp))
-      
+
         return list(Zps - Bps)
-    
+
     def __str__(self):
         """El toString del complejo."""
         return "Caras: " + str(self.caras)
@@ -552,15 +552,15 @@ if __name__ == "__main__":
     comp1 = Complejo([(0, 1, 2, 3)])
     print("-------------COMP1-------------")
     analisisComplejo(comp1, set((0, 1)))
-    
+
     comp2 = Complejo(list(comp1.k_esqueleto(2)))
     print("\n-------------COMP2-------------")
     analisisComplejo(comp2, set((0,)))
-    
+
     comp3 = Complejo([(0, 1), (1, 2, 3, 4), (4, 5), (5, 6), (4, 6), (6, 7, 8), (8, 9)])
     print("\n-------------COMP3-------------")
     analisisComplejo(comp3, set((4,)))
-    
+
     comp4 = Complejo(list(comp3.k_esqueleto(1)))
     print("\n-------------COMP4-------------")
     analisisComplejo(comp4, set((4,)))
@@ -568,15 +568,15 @@ if __name__ == "__main__":
     comp5 = Complejo([(0, 1, 2), (2, 3), (3, 4)])
     print("\n-------------COMP5-------------")
     analisisComplejo(comp5, set((2,)))
-    
+
     comp6 = Complejo([(1, 2, 4), (1, 3, 6), (1, 4, 6), (2, 3, 5), (2, 4, 5), (3, 5, 6)])
     print("\n-------------COMP6-------------")
     analisisComplejo(comp6, set((1, 4)))
-    
+
     comp7 = Complejo(list(comp6.k_esqueleto(1)))
     print("\n-------------COMP7-------------")
     analisisComplejo(comp7, set((1, 4)))
-    
+
     comp8 = Complejo([(1, 2, 4), (2, 4, 5), (2, 3, 5), (3, 5, 6), (1, 3, 6), (1, 4, 6),
                       (4, 5, 7), (5, 7, 8), (5, 6, 8), (6, 8, 9), (4, 6, 9), (4, 7, 9),
                       (1, 7, 8), (1, 2, 8), (2, 8, 9), (2, 3, 9), (3, 7, 9), (1, 3, 7)])
@@ -604,46 +604,46 @@ if __name__ == "__main__":
     print("\n-------------COMP13-------------")
     comp13 = Complejo()
     comp13.setCaras([(0, 1)], 1.0)
-    
+
     comp13.setCaras([(1, 2), (2, 3), (2, 4)], 2.0)
     comp13.setCaras([(3, 4)], 3.0)
     comp13.setCaras([(2, 3, 4)], 4.0)
-    
+
     # Todas las caras del complejo
     print(f"Todas las caras: {comp13.getCaras()}")
-    
+
     # Umbral
     print(f"Umbral de {{3}}: {comp13.umbral((3,))}")
-    
+
     # Filtraciones
     K1 = comp13.filtracion(1.0)
     K2 = comp13.filtracion(2.0)
     K3 = comp13.filtracion(3.0)
     K4 = comp13.filtracion(4.0)
-    
+
     # Todas las caras de las filtraciones
     print(f"Todas las caras de K1: {K1.getCaras()}")
     print(f"Todas las caras de K2: {K2.getCaras()}")
     print(f"Todas las caras de K3: {K3.getCaras()}")
     print(f"Todas las caras de K4: {K4.getCaras()}")
-    
+
     # Caras ordenedas por filtracion
     print(f"Caras ordenadas segun las filtraciones: {comp13.getCarasOrd()}")
-    
+
     """
-    
+
     """
     points = np.array([(0.38021546727456423, 0.46419202339598786),
                        (0.7951628297672293, 0.49263630135869474),
                        (0.566623772375203, 0.038325621649018426),
-                       (0.3369306814864865, 0.7103735061134965), 
+                       (0.3369306814864865, 0.7103735061134965),
                        (0.08272837815822842, 0.2263273314352896),
                        (0.5180166301873989, 0.6271769943824689),
                        (0.33691411899985035, 0.8402045183219995),
                        (0.33244488399729255, 0.4524636520475205),
                        (0.11778991601260325, 0.6657734204021165),
                        (0.9384303415747769, 0.2313873874340855)])
-    
+
     points = np.array([[0.8957641450573793, 0.2950833519989374],
                        [0.028621391963087994, 0.9440875759025237],
                        [0.517621505875702, 0.1236620161847416],
@@ -654,23 +654,23 @@ if __name__ == "__main__":
                        [0.18407448222466916, 0.7868606964403773],
                        [0.4496777667376678, 0.874366215574117],
                        [0.08225571534539433, 0.616710205071694]])
-    
+
     curva1 = [4 * sy.sin(t), 9 * sy.cos(t)]
     curva2 = [1 + 3 * t**2, t**3 - 2 * t]
     points = puntosCurvaRuido(curva2, t, -2, 2, numPuntos=30)
     print(points)
-    
+
     plt.plot(points[:, 0], points[:, 1], 'ko')
     plt.show()
-    
+
     vor = drawVor(points)
-  
+
     delaunay(points)
-   
+
     alpha = alfaComplejo(points)
     print(alpha)
     i = 0
-    images = [] 
+    images = []
     for valor in alpha.umbrales():
         # print(valor)
         K = alpha.filtracion(valor)
@@ -681,6 +681,6 @@ if __name__ == "__main__":
         images.append(imageio.imread(f"imgTemp/im{i}.png"))
         i += 1
         plt.show()
-    
+
     imageio.mimsave('alphaGif/alpha.gif', images)
     """
